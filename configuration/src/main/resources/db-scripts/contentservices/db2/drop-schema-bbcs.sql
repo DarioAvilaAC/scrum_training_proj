52d9d536ecfd494c9c02c0a23dd5d5a0be39de45
darioavilaac <darionsavila@gmail.com>
2018-06-11 17:16:00 +0000

    initial commit

---
+++ configuration/src/main/resources/db-scripts/contentservices/db2/drop-schema-bbcs.sql
@@ -0,0 +1,14 @@
+
+drop table CONTENT_STREAM;
+
+drop table OBJECT_DATA;
+
+drop table PROPERTY_DATA;
+
+drop table RELATIONSHIPS;
+
+drop table RENDITION;
+
+drop table REPOSITORY_DEFINITION;
+
+drop table LAST_PUBLICATION_DATE;
---
+++ configuration/src/main/resources/db-scripts/contentservices/sqlserver/upgrade_5_6_4_to_5_7_0/upgrade_5.6.4-to-5.7.0.sql
@@ -0,0 +1,6 @@
+create table LAST_PUBLICATION_DATE (
+    id numeric(19,0) identity not null,
+    lastPublicationDate datetime not null,
+    excludeList nvarchar(255),
+    primary key (id)
+);
